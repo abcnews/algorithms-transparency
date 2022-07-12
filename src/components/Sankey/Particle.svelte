c2ccaf0d82a5eee02c4050b38c61106ed8569a44
Julian Fell <hi@jtfell.com>
2022-07-13 09:09:11 +1000

    add files

---
+++ src/components/Sankey/Particle.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let colour: string;
+  export let x: number;
+  export let y: number;
+  export let size: number;
+
+  const SQUARE_STAR_ASPECT_RATIO = 22/19;
+  const SQUARE_STAR_DOWNSIZE = 0.8;
+  const PI = 3.14;
+</script>
+
+{#if colour === '#FF5148'}
+  <image
+    href="/squarestar.svg"
+    width={size * PI * SQUARE_STAR_ASPECT_RATIO * SQUARE_STAR_DOWNSIZE}
+    height={size * PI * SQUARE_STAR_DOWNSIZE}
+    x={x - size * (PI / 2) * SQUARE_STAR_ASPECT_RATIO * SQUARE_STAR_DOWNSIZE}
+    y={y - size * (PI / 2) * SQUARE_STAR_DOWNSIZE}
+  />
+{:else}
+  <circle
+    class="particle"
+    opacity="1"
+    stroke="black"
+    stroke-width="0.5"
+    fill="{colour}"
+    r={size}
+    cx={x}
+    cy={y}
+  />
+{/if}
