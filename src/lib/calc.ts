076e138ea548bee6334c25a08a5eba63156ebf59
Julian Fell <hi@jtfell.com>
2022-07-12 14:41:50 +1000

    add missing file

---
+++ src/lib/calc.ts
@@ -0,0 +1,23 @@
+export interface RiskDist {
+  low: number;
+  med: number;
+  high: number;
+}
+
+export const calcDist = (rate1: number, rate2: number): any => {
+  const ratio = Math.max(0.5, Math.min(2, (rate1 - 0.09) / (rate2 - 0.09)));
+
+  // Set the ratio of high vs low risk people to be equal to the ratio between the rejection rates
+  const high = (1 / 3) * ratio;
+  const low = 2 / 3 - high;
+
+  // // Leave medium-risk dist equal
+
+  // const high = rate1 * 0.7;
+  // const low = rate1 * 0.5;
+
+  const med = 1 - high - low;
+  console.log({ ratio, high, med, low });
+
+  return { low, med, high };
+};
