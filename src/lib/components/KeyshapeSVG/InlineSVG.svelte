d8e2bb1c8349a91d15639b99d7133b44a7370edc
Julian Fell <hi@jtfell.com>
2022-08-04 11:43:10 +1000

    wrap css in guard to only apply after odyssey has run, add spinner for inline SVGs

--- src/lib/components/KeyshapeSVG/InlineSVG.svelte
+++ src/lib/components/KeyshapeSVG/InlineSVG.svelte
@@ -97,7 +97,34 @@
   xmlns="http://www.w3.org/2000/svg"
   class="keyshape-svg"
   bind:innerHTML={svgContent}
+  data-loaded={isLoaded ? 'true' : 'false'}
   {...svgAttrs}
   {...$$restProps}
   contenteditable
 />
+
+<style>
+  svg[data-loaded="false"] {
+    --size: 60px;
+    --color: #000;
+    --duration: 0.75s;
+
+    height: var(--size);
+    width: var(--size);
+    border-color: var(--color) transparent var(--color) var(--color);
+    border-width: calc(var(--size) / 15);
+    border-style: solid;
+    border-image: initial;
+    border-radius: 50%;
+    animation: var(--duration) linear 0s infinite normal none running rotate;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
